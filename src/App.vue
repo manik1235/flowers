70a01946f35d4cdd87e3b4a252dfe47c322ac55d
Andrew Henning-Kolberg <akolberg@squaremouth.com>
2021-05-07 23:36:10 -0400

    Add hexagon example
    Add hexagons from
    https://github.com/vlewin/vue-hexagon-grid
    
    Adding package through yarn failed.

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-hexagon-grid text="Hello World!"></vue-hexagon-grid>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Hexagon from './hexagon'
+// import 'vue-hexagon-grid/dist/vue-hexagon-grid.min.css';
+
+Vue.component('vue-hexagon-grid', Hexagon)
 
 Vue.config.productionTip = false
 
